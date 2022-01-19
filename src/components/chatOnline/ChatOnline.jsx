01935dd903467712f938890048886a1040a1f1a1
Shanaka Asiri <you@example.com>
2022-01-19 14:21:03 +0530

    fifith commit

--- src/components/chatOnline/ChatOnline.jsx
+++ src/components/chatOnline/ChatOnline.jsx
@@ -1,42 +1,50 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 import './chatOnline.css'
 
-export default function ChatOnline() {
+export default function ChatOnline({onlineUsers, currentId, setCurrentChat}) {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+    const [friends, setFriends] = useState([]);
+    const [onlineFriends, setOnlineFriends] = useState([]);
+
+    useEffect(() => {
+        const getFriends = async () => {
+            const res = await axios.get("/users/friends/" + currentId);
+            setFriends(res.data);
+        };
+
+        getFriends();
+    }, [currentId]);
+
+    useEffect(() => {
+        setOnlineFriends(friends.filter((f) => onlineUsers.includes(f._id)));
+    }, [friends, onlineUsers]);
+
+    const handleClick = async (user) => {
+        try{
+            const res = await axios.get(`/conversations/find/${currentId}/${user._id}`);
+
+            setCurrentChat(res.data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
 
     return (
         <div className='chat-online'>
-            <div className="chatOnline-friend">
-                <div className="chatOnlineImg-container">
-                    <img className='chatOnline-img' src={PF + "user/1.jpg"} alt=''></img>
-                    <div className="online-badge"></div>
+            {onlineFriends.map((o) => (
+                <div className="chatOnline-friend" onClick={() => handleClick(o)}>
+                    <div className="chatOnlineImg-container">
+                        <img className='chatOnline-img' src={o?.profilePicture ? PF + o.profilePicture : PF + "user/no-avatar.png"} alt=''></img>
+                        <div className="online-badge"></div>
+                    </div>
+                    <span className='chatOnline-name'>{o?.username}</span>
                 </div>
-                <span className='chatOnline-name'>Shanaka Asiri</span>
-            </div> 
-
-            <div className="chatOnline-friend">
-                <div className="chatOnlineImg-container">
-                    <img className='chatOnline-img' src={PF + "user/1.jpg"} alt=''></img>
-                    <div className="online-badge"></div>
-                </div>
-                <span className='chatOnline-name'>Shanaka Asiri</span>
-            </div> 
-
-            <div className="chatOnline-friend">
-                <div className="chatOnlineImg-container">
-                    <img className='chatOnline-img' src={PF + "user/1.jpg"} alt=''></img>
-                    <div className="online-badge"></div>
-                </div>
-                <span className='chatOnline-name'>Shanaka Asiri</span>
-            </div> 
-
-            <div className="chatOnline-friend">
-                <div className="chatOnlineImg-container">
-                    <img className='chatOnline-img' src={PF + "user/1.jpg"} alt=''></img>
-                    <div className="online-badge"></div>
-                </div>
-                <span className='chatOnline-name'>Shanaka Asiri</span>
-            </div>    
+            ))}
+                        
         </div>
     )
 }
--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -18,15 +18,42 @@ export default function Messenger() {
     const [currentChat, setCurrentChat] = useState(null);
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
-    const [socket, setSocket] = useState(null);
+    const [arrivalMessage, setArrivalMessage] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState([])
+
+    const socket = useRef()
 
     const {user} = useContext(AuthContext);
 
     const scrollRef = useRef();
 
     useEffect(() => {
-        setSocket(io("ws://localhost:8900"));
-    }, [])
+        socket.current = io("ws://localhost:8900");
+
+        socket.current.on("getMessage", (data) => {
+            setArrivalMessage({
+                sender : data.senderId,
+                text : data.text,
+                createdAt : Date.now(),
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        arrivalMessage && 
+           currentChat?.members.includes(arrivalMessage.sender) && 
+           setMessages((prev) => [...prev, arrivalMessage]);
+    }, [arrivalMessage, currentChat]);
+
+    useEffect(() => {
+        socket.current.emit("addUser", user._id);
+
+        socket.current.on("getUsers", (users) => {
+            setOnlineUsers(
+                user.followings.filter((f) => users.some((u) => u.userId === f))
+            );
+        })
+    }, [user]);
 
     useEffect(() => {
         const getConversations = async () => {
@@ -62,6 +89,16 @@ export default function Messenger() {
             text : newMessage,
             conversationId : currentChat._id,
         };
+
+        const receiverId = currentChat.members.find(
+            (member) => member !== user._id
+        );
+
+        socket.current.emit("sendMessage", {
+            senderId : user._id,
+            receiverId,
+            text : newMessage,
+        });
 
         try{
             const res = await axios.post("/messages", message);
@@ -123,7 +160,7 @@ export default function Messenger() {
                 </div>
                 <div className="chat-online">
                     <div className="chatOnline-wrapper">
-                        <ChatOnline></ChatOnline>
+                        <ChatOnline onlineUsers={onlineUsers} currentId={user._id} setCurrentChat={setCurrentChat}></ChatOnline>
                     </div>
                 </div>
             </div>
